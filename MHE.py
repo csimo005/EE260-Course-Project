3f4f83a680fe67f6f0d9fd9c3ce7214d6ed50ca9
Cody Simons <csimo005@ucr.edu>
2020-05-30 19:53:05 -0700

    Tested, should be working now

--- MHE.py
+++ MHE.py
@@ -1,16 +1,13 @@
 import torch
 import torch.nn as nn
 
-def euclidean_dist(w_i, w_j):
-    return torch.norm(w_i-w_j, 2)
-
 def angular_dist(w_i, w_j):
     return torch.acos(torch.sum(w_i*w_j))
 
-def MHE(weights, s, dist_func='euclidean'):
-    if dist_func == 'euclidean':
-        dist = lambda w_i, w_j: euclidean_dist(w_i, w_j)
-    elif dist_func == 'angular':
+def MHE(weights, s, dist='euclidean', device='cpu'):
+    if dist == 'euclidean':
+        dist = lambda W, w_i: torch.norm(W-w_i, 2, dim=1)
+    elif dist == 'angular':
         dist = lambda w_i, w_j: angular_dist(w_i, w_j)
     else:
         raise NotImplementedError
@@ -20,34 +17,34 @@ def MHE(weights, s, dist_func='euclidean'):
     else:
         fs = lambda x: torch.log(torch.pow(x, -1))
 
-    energy = torch.zeros(1)
-    for i in range(weights.shape[0]):
-        for j in range(i, weights.shape[0]): #Assumes that what ever distance metric is symmetric
-            energy += fs(dist(weights[i:i+1], weights[j:j+1]))
+    energy = torch.zeros(1, device=device, requires_grad=True)
+    for i in range(weights.shape[0]-1):
+        energy = energy +torch.sum(fs(dist(weights[i+1:], weights[i])))
 
     energy = (2/(weights.shape[0]*(weights.shape[0]-1)))*energy
     return energy
     
 
-def MHE_halfspace(weights, s):
+def MHE_halfspace(weights, s, dist='euclidean', device='cpu'):
     weights = torch.cat((weights, -weights), 0)
-    return MHW(weights, s)
+    return MHE(weights, s, dist=dist, device=device)
 
-def MHE_loss(model, s, lh, lo):
-    hidden_loss = torch.zeros(1)
-    output_loss = torch.zeros(1)
+def MHE_loss(model, s, lh, lo, dist='euclidean', device='cpu'):
+    hidden_loss = torch.zeros(1, device=device, requires_grad=True)
+    output_loss = torch.zeros(1, device=device, requires_grad=True)
 
-    for name, param in state_dict.named_children():
+    for name, param in model.named_children():
         if type(param) is nn.Linear:
-            weights = torch.cat((param._parameters['weight'], param._parameters['bias'].view(-1,1)), 0)
+            weights = torch.cat((param._parameters['weight'], param._parameters['bias'].view(-1,1)), 1)
         elif type(param) is nn.Conv2d:
             weights = torch.cat((param._parameters['weight'].view(param.out_channels,-1), param._parameters['bias'].view(-1,1)), 0)
         else:
             raise NotImplementedError
 
-        if name == '_output':
-            output_loss = MHE(weights, s)
-        else:
-            hidden_loss += MHE_halfspace(weights, s)
+        weights = weights/torch.norm(weights,2,dim=1).view(-1,1)
 
+        if name == '_output':
+            output_loss = MHE(weights, s, dist=dist, device=device)
+        else:
+            hidden_loss = hidden_loss + MHE_halfspace(weights, s, dist=dist, device=device)
     return lh*hidden_loss + lo*output_loss
